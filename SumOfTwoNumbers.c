01bfeb90bcce5651419bf9e81b5938043754b761
sanjug-sonowal <68654609+sanjug-sonowal@users.noreply.github.com>
2020-10-16 23:12:02 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.c
@@ -0,0 +1,12 @@
+//Write a c program to calculate sum of two numbers?
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a=50;
+    b=50;
+    c=a+b;
+    printf("The Sum of two numbers Valuse is = %d",a+b);
+    return 0;
+}
