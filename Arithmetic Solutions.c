48886a8c97d2ca7b9dc1919de31437050f6bf963
sanjug-sonowal <68654609+sanjug-sonowal@users.noreply.github.com>
2020-10-19 23:15:57 +0530

    Add files via upload

---
+++ Arithmetic Solutions.c
@@ -0,0 +1,23 @@
+//Write a c program to calculate sum of two numbers?
+
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+    printf("Enter the first number \n");
+    scanf("%d",&a);
+
+     printf("Enter the second number \n");
+    scanf("%d",&b);
+
+    printf("Your Addition is = %d \n ",a+b);
+
+    printf("Your multiplication is = %d \n",a*b);
+
+    printf("Your division is = %f \n",a/b);
+
+   printf("Your remainder is = %d \n",a%b);
+
+    printf("Your subtraction is = %d \n",a-b);
+    return 0;
+}
